5d6e3c653b56513f441bef97233045752fd200eb
luiznaiper <luiz_naiper@hotmail.com>
2023-03-01 06:37:42 -0300

    :construction: Adding styles and fixing img

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const pageLinks = [
+  {
+    id: 1,
+    href: '#home',
+    text: 'home',
+  },
+  {
+    id: 2,
+    href: '#about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    href: '#services',
+    text: 'services',
+  },
+  {
+    id: 4,
+    href: '#tours',
+    text: 'tours',
+  },
+]
+
+export const socialLinks = [
+  { id: 1, href: 'https://github.com/luiznaiper', icon: 'fab fa-github' },
+  {
+    id: 2,
+    href: 'https://www.linkedin.com/in/luis-olivarez/',
+    icon: 'fab fa-linkedin',
+  },
+  {
+    id: 3,
+    href: 'https://www.facebook.com/luiznaiper',
+    icon: 'fab fa-facebook',
+  },
+  { id: 4, href: 'https://twitter.com/LuizNaiper', icon: 'fab fa-twitter' },
+]
